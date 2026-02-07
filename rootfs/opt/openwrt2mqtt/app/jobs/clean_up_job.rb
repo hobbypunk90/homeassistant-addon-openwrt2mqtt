4f7bdab123268aaba87514bdbc813aa78a26794e
Marcel Hoppe <hoppe.marcel@gmail.com>
2026-02-07 20:26:39 +0100

    Fix cleanup

--- rootfs/opt/openwrt2mqtt/app/jobs/clean_up_job.rb
+++ rootfs/opt/openwrt2mqtt/app/jobs/clean_up_job.rb
@@ -2,7 +2,7 @@ class CleanUpJob < ApplicationJob
   queue_as :clean_up
 
   def perform
-    WiFiDevice.where.not('labels LIKE \'%"persistent"%\'')
+    WiFiDevice.where.not('labels LIKE \'%"permanent"%\'')
               .and(WiFiDevice.where(updated_at: ..Settings.delete_unseen_after_days.days.ago)
                              .or(WiFiDevice.where(last_seen_at: ..Settings.delete_unseen_after_days.days.ago)))
               .each(&:destroy)
